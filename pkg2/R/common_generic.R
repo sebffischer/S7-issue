b508a26c2ff3da6970df65e8aa4ee1a1c50369c4
Sebastian Fischer <sebf.fischer@gmail.com>
2025-10-21 10:52:51 +0200

    ...

--- pkg2/R/common_generic.R
+++ pkg2/R/common_generic.R
@@ -2,12 +2,13 @@
 class_pkg2 <- S7::new_class("class_pkg2")
 
 #' @importFrom pkg1 common_generic
-#' @export
-common_generic <- common_generic
-
 S7::method(common_generic, class_pkg2) <- function(x) {
   print("Calling from pkg2")
 }
+
+#' @export
+common_generic <- pkg1::common_generic
+
 
 .onLoad <- function(libname, pkgname) {
   S7::methods_register()
